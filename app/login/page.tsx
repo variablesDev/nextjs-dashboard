d5ace512594c5a7fa40fb117c2fb89225aca638b
variablesDev <kazsmer87@gmail.com>
2024-06-29 23:21:26 +0200

    finish chapter

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+};
 
 export default function LoginPage() {
   return (
